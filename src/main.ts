9de968e43b463f8d613d3b0aec606c459fce664f
Milyn <milyng.dsilva@gmail.com>
2022-06-06 22:01:09 +0530

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
@@ -9,6 +10,15 @@ async function bootstrap() {
     allowedHeaders: '*',
     origin: '*',
   });
+
+  const config = new DocumentBuilder()
+  .setTitle('Login')
+  .setDescription('Login using bcrypt')
+  .setVersion('0.1')
+  .addTag('bcrypt')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(4000);
 }
