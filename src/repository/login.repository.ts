3c5aeda14efbc47ec9661ca1f63b9d10c3fe5a03
Milyn <milyng.dsilva@gmail.com>
2022-06-08 22:11:50 +0530

    minor changes

--- src/repository/login.repository.ts
+++ src/repository/login.repository.ts
@@ -43,7 +43,7 @@ export class LoginRepository {
                     return { message: "Invalid Credentials" };
                 }
             } catch(e) {
-                console.log(e)
+                return e;
             }
     } catch(e) {
         return e;
